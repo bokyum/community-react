4c39891a29a578dfe568909c80134b9136cd0def
bokyum <95bogyeom@gmail.com>
2021-07-19 07:05:17 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UserEditPage from './pages/users/UserEditPage';
+import LoginPage from './pages/users/LoginPage';
+import JoinPage from './pages/users/JoinPage';
+import LogoutPage from './pages/users/LogoutPage';
+import UserDeletePage from './pages/users/UserDeletePage';
+import WritePage from './pages/blogs/WritePage';
+import EditPage from './pages/blogs/EditPage';
+import DeletePage from './pages/blogs/DeletePage';
+import SearchPage from './pages/blogs/SearchPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route path="/" component={HomePage} />
+    <Route path="/login" component={LoginPage} />
+    <Route path="/" component={JoinPage} />
+    <Route path="/" component={LogoutPage} />
+    <Route path="/" component={UserEditPage} />
+    <Route path="/" component={UserDeletePage} />
+    <Route path="/" component={WritePage} />
+    <Route path="/" component={EditPage} />
+    <Route path="/" component={SearchPage} />
+    <Route path="/" component={DeletePage} />
+    
+
   );
 }
 
---
+++ src/pages/users/UserDeletePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserDeletePage = () => {
+    return (
+    <div>UserDeletePage</div>
+    );
+}
+
+export default UserDeletePage;
